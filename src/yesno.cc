4673886580db70336892dec9346b8e733cf9f7a8
Tony Pujals <tpujals@gmail.com>
2023-06-27 08:31:58 +0000

    chore: refactor

--- src/yesno.cc
+++ src/yesno.cc
@@ -1,30 +1,31 @@
+// #include <bits/stdc++.h>
 #include <csignal>
 #include <iomanip>
 #include <iostream>
 #include <limits>
 #include <string>
 
-void
-exit_yes() {
+void exit_yes()
+{
 	exit(0);
 }
 
-void
-exit_no() {
+void exit_no()
+{
 	exit(1);
 }
 
-void
-ctrl_signal_handler(int s) {
+void ctrl_signal_handler(int s)
+{
 	std::cout << std::endl;
 	exit_no();
 }
 
-int
-main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 	signal(SIGINT, ctrl_signal_handler);
 	std::string answer;
-	while (1) {
+	for (;;) {
 		std::cout << "Enter (Y)es or (n)o: ";
 		// std::cout << "peek: " << std::cin.peek() << std::endl;
 		if (std::cin.peek() == '\n') exit_yes();
