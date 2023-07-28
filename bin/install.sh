6ac4b0ef5360bb52c1d41ff7cac70a8ac413b4f5
Tony Pujals <tpujals@gmail.com>
2023-07-27 19:42:13 -0700

    feat: add git and configure .gitignore

--- bin/install.sh
+++ bin/install.sh
@@ -4,11 +4,18 @@ set -eu
 sudo apt -y update && sudo apt -y install \
 	build-essential \
 	curl \
+	git \
 	tmux \
 	vim
+
+# git
+touch ~/.gitignore
+git config --global core.excludesfile ~/.gitignore
 
 # https://github.com/nvm-sh/nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
 nvm install node
+# update latest npm patch
+npm install -g npm@latest
 
 sudo update-alternatives --set editor /usr/bin/vim.basic
