72e8c62d5b5c7b2aa60047f1a59ea370272d6638
Tony Pujals <tpujals@gmail.com>
2023-07-27 20:17:06 -0700

    feat: add apt autoremove

--- bin/install.sh
+++ bin/install.sh
@@ -8,12 +8,15 @@ sudo apt -y update && sudo apt -y install \
 	tmux \
 	vim
 
+sudo apt autoremove
+
 # git
 touch ~/.gitignore
 git config --global core.excludesfile ~/.gitignore
 
 # go
 # TODO: grep for latest version and apply to url
+# could just use apt, but it always seems to be one or two releases behind
 sudo rm -rf /usr/local/go && \
 	sudo curl -Lo- https://go.dev/dl/go1.20.6.linux-amd64.tar.gz | \
 	sudo tar -C /usr/local -xzf -
