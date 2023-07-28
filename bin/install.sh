6768e1c9250606587abed5002ab61a07ee7d3d65
Tony Pujals <tpujals@gmail.com>
2023-07-27 20:08:25 -0700

    feat: add go

--- bin/install.sh
+++ bin/install.sh
@@ -12,10 +12,17 @@ sudo apt -y update && sudo apt -y install \
 touch ~/.gitignore
 git config --global core.excludesfile ~/.gitignore
 
+# go
+# TODO: grep for latest version and apply to url
+sudo rm -rf /usr/local/go && \
+	sudo curl -Lo- https://go.dev/dl/go1.20.6.linux-amd64.tar.gz | \
+	sudo tar -C /usr/local -xzf -
+
+# node / nvm
 # https://github.com/nvm-sh/nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
 nvm install node
-# update latest npm patch
 npm install -g npm@latest
 
+# configure defaults
 sudo update-alternatives --set editor /usr/bin/vim.basic
