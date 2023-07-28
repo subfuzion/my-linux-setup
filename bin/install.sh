87515039dffe988b794961c3660a806991daf6f3
Tony Pujals <tpujals@gmail.com>
2023-07-27 19:24:32 -0700

    feat: add nvm and node

--- bin/install.sh
+++ bin/install.sh
@@ -7,5 +7,8 @@ sudo apt -y update && sudo apt -y install \
 	tmux \
 	vim
 
+# https://github.com/nvm-sh/nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
+nvm install node
 
 sudo update-alternatives --set editor /usr/bin/vim.basic
